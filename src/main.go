2b4dc5113fc4ba08b0ea400faa56f210827573b8
Adam Saponara <saponara@gmail.com>
2014-08-13 01:57:29 -0400

    init commit

---
+++ src/main.go
@@ -0,0 +1,264 @@
+package main
+
+import (
+    "time"
+    "math"
+    "flag"
+    "fmt"
+)
+
+var (
+    stations map[string]*Station
+    lines map[string]*ServiceLine
+    trains []*Train
+    simulationMode bool
+    simulationWeekMin int
+    simulationSleepMs int
+    dataDir string
+)
+
+// A station (e.g., WTC)
+type Station struct {
+    Name string
+    Id string
+    Lat float64
+    Lon float64
+}
+
+// A service line (e.g., Newark-WTC)
+type ServiceLine struct {
+    Name string
+    Id string
+    Color1 string
+    Color2 string
+    Platforms []*StationPlatform
+    Waypoints []*Waypoint
+    TotalDistance float64
+    StationStops []*StationStop
+    Route []interface{} // For json decode
+    WeeklySched []int // For json decode
+    Stops []string // For json decode
+}
+
+// A station platform (e.g., the WTC-bound platform @ Grove St on the
+// Newark-WTC line)
+type StationPlatform struct {
+    *Station
+    *ServiceLine
+    *Waypoint
+    Prev *StationPlatform
+    Trains []*Train
+}
+
+// A stop at a certain platform (e.g., the weekday 10:21am WTC-bound stop at
+// Grove St)
+type StationStop struct {
+    Platform *StationPlatform
+    Next *StationStop
+    WeekMin int // minute of week (0 thru 10079)
+}
+
+// A point on a map with pointers to next/prev points
+type Waypoint struct {
+    Lat float64
+    Lon float64
+    Platform *StationPlatform
+}
+
+// A train
+type Train struct {
+    Id int
+    CurStop *StationStop
+    CurProgress float64
+    Terminated bool
+    Updated int
+    Lat float64
+    Lon float64
+}
+
+func main() {
+    // Flags
+    flag.BoolVar(&simulationMode, "s", true, "enable simulation mode")
+    flag.IntVar(&simulationSleepMs, "m", 100, "num ms to sleep in simulation mode")
+    flag.IntVar(&simulationWeekMin, "w", 0, "weekMin to start at in simulation mode")
+    flag.StringVar(&dataDir, "d", "../res", "data directory")
+    flag.Parse()
+    simulationWeekMin -= 1
+
+    // Parse data
+    if err := buildResources(dataDir); err != nil { panic(err) }
+
+    // Run trains
+    weekMin := 0
+    lastWeekMin := 99
+    for {
+        // TODO No idea what is supposed to happen during DST
+        if weekMin = getCurWeekMin(); weekMin != lastWeekMin {
+            // Check for station stops that just occurred
+            for _, line := range lines {
+                for _, stop := range line.GetStationStops(weekMin) {
+                    fmt.Printf("[%s] Line %s stop at %s\n", weekMinToStr(weekMin), stop.Platform.ServiceLine.Name, stop.Platform.Station.Name)
+                    // Move train form previous platform this platform
+                    var train *Train
+                    if train = stop.Platform.Prev.DequeueTrain(weekMin); train == nil {
+                        // No train was there, so make a new train!
+                        train = getOrCreateNewTrain(stop.Platform.Station)
+                    }
+                    train.CurStop = stop
+                    train.CurProgress = 0.0
+                    train.Terminated = train.CurStop.Next == nil
+                    train.Updated = weekMin
+                    stop.Platform.PushTrain(train)
+                }
+            }
+            lastWeekMin = weekMin
+        }
+
+        // Update train progress and position
+        updateTrains(weekMin)
+
+        if simulationMode {
+            // Sleep a tiny bit
+            time.Sleep(time.Duration(simulationSleepMs) * time.Millisecond)
+        } else {
+            // Sleep a bit
+            time.Sleep(1 * time.Second)
+        }
+    }
+}
+
+// Get a `Terminated` train at `station` or create a new one
+func getOrCreateNewTrain(station *Station) *Train {
+    for _, train := range trains {
+        //if train.Terminated && train.CurStop.Platform.Station == station {
+        if train.Terminated {
+            return train
+        }
+    }
+    train := new(Train)
+    train.Id = len(trains)
+    trains = append(trains, train)
+    fmt.Printf("Spawned new train #%d at station %s\n", train.Id, station.Name)
+    return train
+}
+
+// Get current minute of week
+func getCurWeekMin() int {
+    if simulationMode {
+        simulationWeekMin += 1
+        if simulationWeekMin >= 10080 {
+            simulationWeekMin = 0
+        }
+        return simulationWeekMin
+    } else {
+        hour, min, _ := time.Now().Clock()
+        return hour*60 + min
+    }
+}
+
+// Get difference between two `weekMin` values, compensating for the 11:59pm
+// to 12:01am case
+func weekMinDiff(future, now int) int {
+    if future < now {
+        future += 60*24*7
+    }
+    return future-now
+}
+
+// Update progress of trains
+func updateTrains(weekMin int) {
+    for _, train := range trains {
+        if !train.Terminated {
+            // Update progress til next stop
+            train.CurProgress = float64(weekMinDiff(weekMin, train.CurStop.WeekMin)) /
+                float64(weekMinDiff(train.CurStop.Next.WeekMin, train.CurStop.WeekMin))
+            // Update position
+            train.updatePosition()
+        }
+    }
+}
+
+// Update a train's geographical position
+func (train *Train) updatePosition() {
+    if train.Terminated {
+        return
+    }
+    line := train.CurStop.Platform.ServiceLine
+    targetDistance := train.CurProgress*line.TotalDistance
+
+    distanceTally := 0.0
+    for i := 0; i < len(line.Waypoints) - 1; i += 1 {
+        waypoint := line.Waypoints[i]
+        nextWaypoint := line.Waypoints[i+1]
+        waypointDistance := waypoint.DistanceTo(nextWaypoint)
+        if distanceTally + waypointDistance >= targetDistance {
+            baseDistance := distanceTally
+            distanceTally += waypointDistance
+            factor := (targetDistance-baseDistance)/(distanceTally-baseDistance)
+            // TODO This does not take into account Earth curvature
+            train.Lat = (nextWaypoint.Lat+waypoint.Lat)*factor
+            train.Lon = (nextWaypoint.Lon+waypoint.Lon)*factor
+            return
+        }
+    }
+
+    // This should not happen
+    train.Lat = train.CurStop.Next.Platform.Waypoint.Lat
+    train.Lon = train.CurStop.Next.Platform.Waypoint.Lon
+}
+
+// Dequeue a train from `platform`. Only consider trains that have an
+// `Updated` value less than `weekMin`.
+func (platform *StationPlatform) DequeueTrain(weekMin int) *Train {
+    if platform == nil || len(platform.Trains) < 1 {
+        return nil
+    }
+    indexToPop := -1
+    for i := len(platform.Trains) - 1; i >= 0; i-- {
+        train := platform.Trains[i]
+        if train.Updated < weekMin {
+            indexToPop = i
+            break
+        }
+    }
+    if indexToPop != -1 {
+        train := platform.Trains[indexToPop]
+        platform.Trains = append(platform.Trains[:indexToPop], platform.Trains[indexToPop+1:]...)
+        return train
+    }
+    return nil
+}
+
+// Queue a train to `platform`
+func (platform *StationPlatform) PushTrain(train *Train) {
+    platform.Trains = append([]*Train{train}, platform.Trains[:]...)
+    fmt.Printf("Train %d now at %s\n", train.Id, platform.Station.Name)
+}
+
+// Get station stop that occur at `weekMin`
+func (line *ServiceLine) GetStationStops(weekMin int) []*StationStop {
+    stops := make([]*StationStop, 0)
+    for _, stop := range line.StationStops {
+        // TODO Sort stops and use binary search
+        if stop.WeekMin == weekMin {
+            stops = append(stops, stop)
+        }
+    }
+    return stops
+}
+
+// Return distance between `a` and `b`
+func (a *Waypoint) DistanceTo(b *Waypoint) float64 {
+    // TODO This does not take into account Earth curvature
+    return math.Sqrt(
+        math.Pow(b.Lat - a.Lat, 2) +
+        math.Pow(b.Lon - a.Lon, 2))
+}
+
+func weekMinToStr(weekMin int) string {
+    day := weekMin / 1440
+    dayMin := weekMin % 1440
+    hour := dayMin / 60
+    min := dayMin % 60
+    return fmt.Sprintf("day=%d %d:%02d", day, hour, min)
+}
---
+++ src/res.go
@@ -0,0 +1,195 @@
+package main
+
+import (
+    "fmt"
+    "path/filepath"
+    "encoding/json"
+    "errors"
+    "strings"
+    "strconv"
+    "os"
+)
+
+type routePoint struct {
+    *Station
+    Coord float64
+}
+
+// Read resources files under `path`
+func buildResources(path string) error {
+    var err error
+
+    // Stations
+    stationFiles, err := filepath.Glob(fmt.Sprintf("%s/stations/%s", path, "*"))
+    if err != nil { return err }
+    err = buildStations(stationFiles)
+    if err != nil { return err }
+
+    // Lines
+    lineFiles, err := filepath.Glob(fmt.Sprintf("%s/lines/%s", path, "*"))
+    if err != nil { return err }
+    err = buildServiceLines(lineFiles)
+    if err != nil { return err }
+
+    return nil
+}
+
+// Build stations
+func buildStations(files []string) error {
+    stations = make(map[string]*Station)
+    for _, file := range files {
+        err := func() error {
+            fd, err := os.Open(file)
+            if err != nil { return err }
+            defer fd.Close()
+            station := new(Station)
+            decoder := json.NewDecoder(fd)
+            if err = decoder.Decode(station); err != nil {
+                return err
+            }
+            stations[station.Id] = station
+            return nil
+        }()
+        if err != nil { return err }
+    }
+    return nil
+}
+
+// Build lines
+func buildServiceLines(files []string) error {
+    lines = make(map[string]*ServiceLine)
+    for _, file := range files {
+        err := func() error {
+            fd, err := os.Open(file)
+            if err != nil { return err }
+            defer fd.Close()
+            line := new(ServiceLine)
+            decoder := json.NewDecoder(fd)
+            if err = decoder.Decode(line); err != nil { return err }
+            if err = line.parseRoute(); err != nil { return err }
+            if err = line.parseSched(); err != nil { return err }
+            lines[line.Id] = line
+            return nil
+        }()
+        if err != nil { return err }
+    }
+    return nil
+}
+
+func (line *ServiceLine) parseSched() error {
+    // Parse stops
+    dayMins := make([]int, 0, len(line.Stops))
+    isPm := false
+    lastHour := -1
+    var hour, adjHour, min int
+    var err error
+    for _, stop := range(line.Stops) {
+        hourMin := strings.SplitN(stop, ":", 2)
+        if len(hourMin) != 2 {
+            return errors.New("Expected h:mm or hh:mm format")
+        }
+        if hour, err = strconv.Atoi(hourMin[0]); err != nil {
+            return errors.New("Expected hour to be a number")
+        }
+        if min, err = strconv.Atoi(hourMin[1]); err != nil {
+            return errors.New("Expected min to be a number")
+        }
+        adjHour = hour
+        if hour == 12 && lastHour == 11 {
+            isPm = !isPm
+        }
+        if !isPm && hour == 12 {
+            adjHour = 0
+        } else if isPm && hour != 12 {
+            adjHour += 12
+        }
+        lastHour = hour
+        dayMins = append(dayMins, adjHour*60 + min)
+    }
+
+    // Set line.StationStops
+    line.StationStops = make([]*StationStop, 0, len(dayMins)*7)
+    var prevStationStop *StationStop
+    for i := 0; i < len(line.WeeklySched); i += 2 {
+        startMin := line.WeeklySched[i]
+        endMin := line.WeeklySched[i+1]
+        if startMin == -1 || endMin == -1 {
+            prevStationStop = nil
+            continue
+        }
+        for j, dayMin := range dayMins {
+            if dayMin < startMin && dayMin > endMin {
+                continue
+            }
+            platform := line.Platforms[j % len(line.Platforms)]
+            stationStop := &StationStop{
+                Platform: platform,
+                WeekMin: dayMin + (i/2)*1440 }
+            if prevStationStop != nil && j%len(line.Platforms) != 0 {
+                prevStationStop.Next = stationStop
+            }
+            line.StationStops = append(line.StationStops, stationStop)
+            prevStationStop = stationStop
+        }
+    }
+    return nil
+}
+
+func (line *ServiceLine) parseRoute() error {
+    routePoints := []routePoint{}
+    for _, route := range line.Route {
+        switch val := route.(type) {
+            case string:
+                if station, ok := stations[val]; !ok {
+                    return errors.New(fmt.Sprintf("Invalid station id %s", val, line.Id))
+                } else {
+                    routePoints = append(routePoints, routePoint{Station: station})
+                }
+            case float64:
+                routePoints = append(routePoints, routePoint{Coord: val})
+            default:
+                return errors.New(fmt.Sprintf("Unexpected %T in Waypoints array", val, line.Id))
+        }
+    }
+
+    // Set line.Waypoints
+    var prevStationPlatform *StationPlatform
+    for i := 0; i < len(routePoints); i++ {
+        routePoint := routePoints[i]
+        waypoint := &Waypoint{}
+        if routePoint.Station == nil {
+            waypoint.Lat = routePoint.Coord
+            i += 1
+            if i >= len(routePoints) {
+                return errors.New("Expected Lon after Lat coordinate")
+            }
+            waypoint.Lon = routePoints[i].Coord
+        } else {
+            waypoint.Lat = routePoint.Station.Lat
+            waypoint.Lon = routePoint.Station.Lon
+            waypoint.Platform = &StationPlatform{
+                Station: routePoint.Station,
+                ServiceLine: line,
+                Waypoint: waypoint,
+                Prev: prevStationPlatform}
+            waypoint.Platform.Trains = make([]*Train, 0)
+            prevStationPlatform = waypoint.Platform
+            line.Platforms = append(line.Platforms, waypoint.Platform)
+        }
+        line.Waypoints = append(line.Waypoints, waypoint)
+    }
+
+fmt.Printf("%+v\n", line.Platforms)
+
+    // Set line.TotalDistance
+    var totalDistance float64
+    for i, waypoint := range line.Waypoints {
+        if i == 0 {
+            continue
+        }
+        totalDistance += line.Waypoints[i].DistanceTo(waypoint)
+    }
+    line.TotalDistance = totalDistance
+
+    return nil
+}
